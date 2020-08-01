eede2113f08f2da4f06f9cb9fe508b1ceb9a1cc1
Abhishek Srikanth <abhisrik99@outlook.com>
2020-08-01 15:15:30 +0530

    Removed unwanted JS code

--- script.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $( 'div.navbar-nav a' ).on( 'click', function() {
-          $( this ).parent().find( 'a.active' ).removeClass( 'active' );
-          $( this ).addClass( 'active' );
-    });
-});
